268148050eccb17626b26135377994c8c1b1701d
avidms <avram.dames@gmail.com>
2021-11-26 19:09:35 +0200

    add router and results page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,10 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
-import ClassCard from "./components/ClassCard.vue";
-import SearchBox from "./components/SearchBox.vue";
 </script>
 
 <template>
   <Navbar class=""></Navbar>
-
-  <div class="flex flex-col">
-    <div class="mt-32">
-      <h1 class="px-4 text-4xl">Descoperă cursul potrivit pentru tine</h1>
-      <SearchBox class="mt-12"></SearchBox>
-    </div>
-
-    <div class="mt-32">
-      <h2 class="text-center">Categorii</h2>
-      <div class="flex space-x-4 justify-center mt-8">
-        <div class="flex bg-red-200 w-40 h-40 justify-center items-center">
-          <div class="border">IT</div>
-        </div>
-        <div class="flex bg-yellow-200 w-40 h-40 justify-center items-center">
-          <div class="border">Beauty</div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <router-view></router-view>
 </template>
 
 <style>
---
+++ src/pages/SearchResults.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ClassCard from "../components/ClassCard.vue";
+import SearchResultFilterTagsVue from "../components/SearchResultFilterTags.vue";
+import SearchResultFilterTags from "../components/SearchResultFilterTags.vue";
+</script>
+
+<template>
+  <div>
+      <SearchResultFilterTags></SearchResultFilterTags>
+  </div>
+  <div class="mt-8">
+    <ClassCard></ClassCard>
+    <ClassCard></ClassCard>
+    <ClassCard></ClassCard>
+  </div>
+</template>

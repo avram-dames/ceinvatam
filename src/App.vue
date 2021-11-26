6b56e4f5554ea794fd885a1df2ce3b7c2b53169b
avidms <avram.dames@gmail.com>
2021-11-26 16:35:42 +0200

    Adaugare multiselect

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <script setup>
-import Navbar from './components/Navbar.vue'
-import ClassCard from './components/ClassCard.vue'
-import SearchBox from './components/SearchBox.vue';
+import Navbar from "./components/Navbar.vue";
+import ClassCard from "./components/ClassCard.vue";
+import SearchBox from "./components/SearchBox.vue";
 </script>
 
 <template>
-  <Navbar></Navbar>
-  <h1>Descoperă cursul potrivit pentru tine</h1>
-  <SearchBox></SearchBox>
-  <ClassCard></ClassCard>
+  <Navbar class=""></Navbar>
+
+  <div class="flex flex-col">
+    <div class="mt-32">
+      <h1 class="px-4 text-4xl">Descoperă cursul potrivit pentru tine</h1>
+      <SearchBox class="mt-12"></SearchBox>
+    </div>
+
+    <div class="mt-32">
+      <h2 class="text-center">Categorii</h2>
+      <div class="flex space-x-4 justify-center mt-8">
+        <div class="flex bg-red-200 w-40 h-40 justify-center items-center">
+          <div class="border">IT</div>
+        </div>
+        <div class="flex bg-yellow-200 w-40 h-40 justify-center items-center">
+          <div class="border">Beauty</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
 #app {
-
 }
 </style>
--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,28 +1,18 @@
 <script setup>
 import { ref } from "vue";
-import Multiselect from '@vueform/multiselect'
-
-components: {Multiselect}
-
-const value = ref(null)
-const options =  ['list', 'of', 'options']
-
+import SearchBoxMultiSelect from "./SearchBoxMultiSelect.vue";
 </script>
 
-<template>
-
-<div>
-    <multiselect
-      v-model="value"
-      mode="multiple"
-      placeholder="Select your characters"
-      :options="options"
-    >
-      <template v-slot:multipleLabel="{ values }">
-        <div class="multiselect-multiple-label">
-          {{ values.length }} characters selected
-        </div>
-      </template>
-    </multiselect>
-  </div>
+<template>  
+  <form action="" class="flex flex-col lg:space-x-2 space-y-4 lg:space-y-0 px-4 lg:flex-row">
+    <input
+      type="text"
+      placeholder="ce dorești să înveți?"
+      class="px-4 py-2 border border-gray-400 rounded-md lg:w-1/3 h-12"
+    />
+    <div class="lg:w-1/3">
+    <SearchBoxMultiSelect></SearchBoxMultiSelect>
+    </div>
+    <button class="px-4 py-2 bg-green-400 rounded-md lg:w-48 h-12">Caută</button>
+  </form>
 </template>

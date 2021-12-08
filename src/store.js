569d3f2c97962d2cf5de7bd9b88c14375c9e64f1
avidms <avram.dames@gmail.com>
2021-12-08 17:21:09 +0200

    connect client app with backend via cloudflare workers and supabase database

--- src/store.js
+++ src/store.js
@@ -4,7 +4,43 @@ const store = {
     debug: true,
 
     state: reactive({
-        searchResults: []
+        searchResults: [],
+        cities: [{
+                id: 1,
+                name: 'București'
+            },
+            {
+                id: 2,
+                name: 'Timișoara'
+            },
+            {
+                id: 3,
+                name: 'Cluj Napoca'
+            },
+            {
+                id: 4,
+                name: 'Chișinău'
+            },
+        ],
+        cityOptions: {
+            1: 'București',
+            2: 'Timișoara',
+            3: 'Cluj Napoca',
+            4: 'Chișinău'
+        },
+        targetUsers: {
+            0: "",
+            1: "Copii",
+            2: "Adulți",
+            3: "Toate Vârstele",
+        },
+        durationUnits: {
+            0: "",
+            1: "ore",
+            2: "luni",
+            3: "ani",
+            4: "perioană nedeterminată",
+        }
     }),
 
     setSearchResults(newResults) {
@@ -21,6 +57,30 @@ const store = {
         }
 
         this.state.searchResults = []
+    },
+
+    setCityFilter(newResults) {
+        if (store.debug) {
+            console.log(`setCityFilter triggered with ${newResults}`);
+
+            this.state.cityFilter = newResults
+        }
+    },
+
+    clearCityFilter() {
+        if (store.debug) {
+            console.log('clearCityFilter triggered')
+        }
+
+        this.state.cityFilter = []
+    },
+
+    getCityIds() {
+        Object.keys(this.state.cityOptions)
+    },
+
+    getCityOptions() {
+        return this.state.cityOptions
     }
 }
 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue()],
+    server: {
+        proxy: {
+            '/query': 'https://ceinvatam-net-workers.lalatech.workers.dev/',
+            // '/query': 'http://127.0.0.1:8787/',
+        }
+    }
 })

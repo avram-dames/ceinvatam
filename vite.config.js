fb09c109f20713e475aabdc4722e304c929fbb6d
avidms <avram.dames@gmail.com>
2021-12-30 16:26:44 +0200

    refactor vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const host = 'http://127.0.0.1:8787';
+// const host = 'https://ceinvatam.avram.dev'
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
     server: {
         proxy: {
-            // '/api/query': 'http://127.0.0.1:8787',
-            // '/api/searchsuggestions': 'http://127.0.0.1:8787',
-            '/api/query': 'https://ceinvatam.avram.dev',
-            '/api/searchsuggestions': 'https://ceinvatam.avram.dev',
+            '/api/query': host,
+            '/api/searchsuggestions': host,
         }
     }
 })

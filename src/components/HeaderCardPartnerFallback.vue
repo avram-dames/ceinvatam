359c67ee4db66bea1f3d48dd4b50980da1a26590
avidms <avram.dames@gmail.com>
2022-01-11 11:41:34 +0200

    add partner page fallback template

---
+++ src/components/HeaderCardPartnerFallback.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="px-4 mt-4">
+    <!-- Title -->
+    <div>
+      <h1 class="h-8 w-40 bg-gray-400"></h1>
+      <p class="mt-2 h-8 w-40 bg-gray-300"></p>
+    </div>
+    
+    <!-- CTA -->
+    <div class="mt-8 flex justify-between">
+      <div>
+        <span class="text-white px-2 py-1 bg-purple-700 rounded-md w-8 w-12"></span>
+      </div>
+
+      <button class="px-2 py-1 bg-green-200 rounded-md h-8 w-32"></button>
+    </div>
+
+    <!-- Info -->
+    <div class="mt-16">
+      <h2 class="h-8 w-40 bg-gray-400"></h2>
+      <p class="mt-4 h-64 w-full bg-gray-300">
+      </p>
+    </div>
+  </div>
+</template>

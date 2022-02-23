4411f69407157ad21433f3425fc17a043a3720d7
avidms <avram.dames@gmail.com>
2022-02-23 16:57:25 +0200

    update my reviews to see partner reviews

--- src/pages/CustomerReviews.vue
+++ src/pages/CustomerReviews.vue
@@ -11,20 +11,25 @@ import ReviewCard from "../components/ReviewCard.vue";
 const store = useStore();
 const router = useRouter();
 
-const reviews = ref();
+const classReviews = ref();
+const partnerReviews = ref();
 
-async function getData() {
+async function getClassReviews() {
   let { data: class_reviews, error } = await supabase
     .from("class_reviews")
     .select("id, text, score, created_at, first_name, avatar_url, class_id")
     .eq("user_id", store.state.user.id);
 
-  if (class_reviews.length === 0) {
-    downgradeUserFromReviewer()
-    router.push({ name: 'Home' })
-  }
+  classReviews.value = class_reviews;
+}
 
-  reviews.value = class_reviews;
+async function getPartnerReviews() {
+  let { data: partner_reviews, error } = await supabase
+    .from("partner_reviews")
+    .select("id, text, score, created_at, first_name, avatar_url, partner_id")
+    .eq("user_id", store.state.user.id);
+
+  partnerReviews.value = partner_reviews;
 }
 
 async function downgradeUserFromReviewer() {
@@ -32,10 +37,10 @@ async function downgradeUserFromReviewer() {
     data: { is_reviewer: false },
   });
 
-  if (error) throw error
+  if (error) throw error;
 }
 
-async function deleteReview(reviewId) {
+async function deleteClassReview(reviewId) {
   const { data, error } = await supabase
     .from("class_reviews")
     .delete()
@@ -43,25 +48,102 @@ async function deleteReview(reviewId) {
 
   if (error) throw error;
 
-  getData();
+  getClassReviews();
 }
 
-function editReview(class_id, review_id) {
-  router.push({ path: `/details/class/${class_id}/review/${review_id}/update` })
+async function deletePartnerReview(reviewId) {
+  const { data, error } = await supabase
+    .from("partner_reviews")
+    .delete()
+    .eq("id", reviewId);
+
+  if (error) throw error;
+
+  getPartnerReviews();
 }
 
-getData();
+function editClassReview(class_id, review_id) {
+  router.push({
+    name: "ReviewClassUpdate",
+    params: { class_id: class_id, id: review_id },
+  });
+}
+
+function editPartnerReview(partner_id, review_id) {
+  router.push({
+    name: "ReviewPartnerUpdate",
+    params: { partner_id: partner_id, id: review_id },
+  });
+}
+
+getClassReviews();
+getPartnerReviews();
+
+if (classReviews.value) {
+  console.log(classReviews.value.length)
+  // downgradeUserFromReviewer();
+  // router.push({ name: "Home" });
+}
 </script>
 
 <template>
   <Navbar class=""></Navbar>
   <Suspense>
     <template #default>
-      <div class="p-4 flex flex-col space-y-4 divide-y divide-solid">
-        <div v-for="review in reviews" :key="review.id">
+      <div class="p-4">
+        <h2>Recenzii Cursuri</h2>
+        <div class="flex flex-col space-y-4 divide-y divide-solid">
+          <div v-for="review in classReviews" :key="review.id">
+            <ReviewCard v-bind="review"></ReviewCard>
+            <div class="flex justify-end space-x-4">
+              <button
+                id="edit-icon"
+                @click="editReview(review.class_id, review.id)"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-8 w-8"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                  />
+                </svg>
+              </button>
+              <button class="delete-icon" @click="deleteReview(review.id)">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-8 w-8"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <h2 class="pt-8">Recenzii Academii</h2>
+        <div class="flex flex-col space-y-4 divide-y divide-solid"></div>
+        <div v-for="review in partnerReviews" :key="review.id">
           <ReviewCard v-bind="review"></ReviewCard>
           <div class="flex justify-end space-x-4">
-            <button id="edit-icon" @click="editReview(review.class_id, review.id)">
+            <button
+              id="edit-icon"
+              @click="editReview(review.class_id, review.id)"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 class="h-8 w-8"

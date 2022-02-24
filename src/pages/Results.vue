ddec6b771964aea9685fb1c244e22d273935c015
avidms <avram.dames@gmail.com>
2022-02-24 11:59:35 +0200

    refactor results page and components

--- src/pages/Results.vue
+++ src/pages/Results.vue
@@ -1,21 +1,22 @@
 <script setup>
-import { computed, ref } from "vue"
+import { computed, ref } from "vue";
 import { useStore } from "vuex";
 
 import Navbar from "../components/Navbar.vue";
-import ClassCard from "../components/ClassCard.vue"
-import SearchResultFilterTags from "../components/SearchResultFilterTags.vue"
+import DetailsClassCard from "../components/DetailsClassCard.vue";
+import ResultsSortersAndFilters from "../components/ResultsSortersAndFilters.vue";
 
-const store = useStore()
-const results = computed(() => store.getters.searchResults)
+const store = useStore();
+const results = computed(() => store.getters.searchResults);
 </script>
 
 <template>
-<Navbar class=""></Navbar>
-  <div>
-      <SearchResultFilterTags></SearchResultFilterTags>
-  </div>
-  <div class="mt-8">
-      <ClassCard v-for="result in results" :key="result.id" v-bind="result"></ClassCard>
-  </div>
+  <Navbar></Navbar>
+  <ResultsSortersAndFilters></ResultsSortersAndFilters>
+  <DetailsClassCard
+    v-for="result in results"
+    :key="result.id"
+    v-bind="result"
+    class="mt-8"
+  ></DetailsClassCard>
 </template>

de98c39f7c420d11a57fc938e861cb62f077ad53
avidms <avram.dames@gmail.com>
2022-03-04 13:50:26 +0200

    implement alerts for pasword reset

--- src/pages/AuthPasswordForgot.vue
+++ src/pages/AuthPasswordForgot.vue
@@ -4,20 +4,56 @@ import { useRouter } from "vue-router";
 import supabase from "../utils/supabase";
 
 import NavbarPlaceholder from "../components/NavbarPlaceholder.vue";
+import AlertModal from "../components/AlertModal.vue";
+import AlertError from "../components/AlertError.vue";
 
 const router = useRouter();
-const props = defineProps(['query'])
+const props = defineProps(["query"]);
+const resetPasswordFormAlertMessage = ref("");
+
+const alertModal = ref({
+  isOpen: false,
+  title: "",
+  message: "",
+});
+
+function openAlertModal(title, message) {
+  alertModal.value.isOpen = true;
+  alertModal.value.title = title;
+  alertModal.value.message = message;
+}
+
+function returnToPrevPage() {
+  router.go(-1);
+}
 
 const form = ref({
   email: props.query,
 });
+
+function handleError(error, message) {
+  // alertMessages.value.push(message);
+  // send error somewhere
+  console.log(message);
+  throw error;
+}
 
 async function submitPasswordResetRequest() {
   let { data, error } = await supabase.auth.api.resetPasswordForEmail(
     form.value.email
   );
 
-  if (error) { alert(error.message); throw error; }
+  if (error) {
+    if (error.status === 404) {
+      resetPasswordFormAlertMessage.value = "Nu există user cu acest email";
+    } else if (error.status === 429) {
+      resetPasswordFormAlertMessage.value =
+        "Trebuie să așteptați 60 de secunde pentru o nouă cerere.";
+    } else {
+      openAlertModal("Conexiune nereușită", "Ne pare rău dar");
+    }
+    handleError(error);
+  }
 
   router.push({ name: "PasswordResetEmailSent" });
 }
@@ -31,7 +67,9 @@ async function submitPasswordResetRequest() {
     <form @submit.prevent="submitPasswordResetRequest()">
       <!-- Email Input -->
       <div class="flex flex-col space-y-2 mt-12">
-        <label for="email">Adresa de email pentru care doriți să resetați parola</label>
+        <label for="email"
+          >Adresa de email pentru care doriți să resetați parola</label
+        >
         <input
           type="email"
           name="email"
@@ -42,10 +80,19 @@ async function submitPasswordResetRequest() {
           placeholder="name@email.com"
         />
       </div>
+
+      <AlertError :message="resetPasswordFormAlertMessage"></AlertError>
+
       <!-- Sign In -->
-      <button class="p-2 bg-red-500 text-white rounded-md mt-8 w-full">
+      <button class="p-2 mt-4 bg-red-500 text-white rounded-md w-full">
         Resetează Parola
       </button>
     </form>
+    <!-- Black overlay in order to make the modal pop -->
+    <div
+      v-if="alertModal.isOpen"
+      class="bg-black z-10 opacity-80 w-full h-screen absolute top-0 left-0"
+    ></div>
   </div>
+  <AlertModal class="z-50" v-bind="alertModal"></AlertModal>
 </template>
